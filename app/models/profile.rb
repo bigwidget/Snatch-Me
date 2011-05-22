3ffb4d1af3f134879d01fe2b1c4cc237b48056a5
Tim Koelkebeck <tim@mytype.com>
2011-05-22 11:52:25 -0400

    cleanse

--- app/models/profile.rb
+++
@@ -1,7 +0,0 @@
-class Profile < ActiveRecord::Base
-
-  has_attached_file :picture,
-                    :default_url => '/images/default_picture.jpg',
-                    :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
-end
